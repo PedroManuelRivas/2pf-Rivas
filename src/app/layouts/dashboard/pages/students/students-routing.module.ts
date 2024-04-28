ab1364b03ec80ae94eef702cd90875d41d046165
privas1 <privas1@bancoestado.cl>
2024-04-28 19:48:12 -0400

    ADD: Enrutamiento

--- src/app/layouts/dashboard/pages/students/students-routing.module.ts
+++ src/app/layouts/dashboard/pages/students/students-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {
+    path: 'students',
+    component: StudentsComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
